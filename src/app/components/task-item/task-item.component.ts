8408cd3dff994477621c445c611406d45bf6e28e
Vincent Nguyen <vnvnguyen3@yahoo.com>
2021-05-11 22:13:09 -0700

    following traversy media

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onDelete(task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task) {
+    this.onToggleReminder.emit(task);
+  }
+
+}
